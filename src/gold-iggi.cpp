5880d6fe9271b344564bed899e3cc0ffea81c8bc
Joseph Walton-Rivers <webpigeon@unitycoders.co.uk>
2014-11-18 20:58:56 +0000

    basic SDL example

--- src/gold-iggi.cpp
+++ src/gold-iggi.cpp
@@ -7,9 +7,32 @@
 //============================================================================
 
 #include <iostream>
+#include <SDL/SDL.h>
 using namespace std;
 
 int main() {
+	 //Start SDL
+	SDL_Init( SDL_INIT_EVERYTHING );
+
 	cout << "Hello, IGGI!" << endl; // prints Hello, IGGI!
+	SDL_Surface* screen = SDL_SetVideoMode( 640, 480, 32, SDL_SWSURFACE );
+
+	bool quit = false;
+
+	//Pause
+	SDL_Event e;
+	while(!quit) {
+		 //Handle events on queue
+		while( SDL_PollEvent( &e ) != 0 ) {
+			//User requests quit
+			if( e.type == SDL_QUIT ) {
+				quit = true;
+			}
+		}
+	}
+
+	//Quit SDL
+	SDL_Quit();
+
 	return 0;
 }
