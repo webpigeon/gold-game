ff3fe1105c6991026ecd4c1d5adeb45bd8aee1a3
Joseph Walton-Rivers <webpigeon@unitycoders.co.uk>
2014-11-18 20:27:51 +0000

    eclipse based project (much simpiler)

---
+++ src/gold-iggi.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : gold-iggi.cpp
+// Author      : Joseph Walton-Rivers & Piers Williams
+// Version     :
+// Copyright   : Copyright (c), 2014. All rights reserved.
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Hello, IGGI!" << endl; // prints Hello, IGGI!
+	return 0;
+}
