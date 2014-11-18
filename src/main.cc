bbb95d0269070b4dfa1e8e6a39f1b21b5997733c
Joseph Walton-Rivers <webpigeon@unitycoders.co.uk>
2014-11-18 19:59:17 +0000

    c++ stuff generated by anjuta

---
+++ src/main.cc
@@ -0,0 +1,15 @@
+/* -*- Mode: C; indent-tabs-mode: t; c-basic-offset: 4; tab-width: 4 -*-  */
+/*
+ * main.cc
+ * Copyright (C) 2014 Joseph Walton-Rivers & Piers Williams <jwalto@essex.ac.uk & pwillc@essex.ac.uk>
+ * 
+ */
+
+#include <iostream>
+
+int main()
+{
+	std::cout << "Hello world!" << std::endl;
+	return 0;
+}
+
