f7c663ea3dd773ef66985961929fdb54f82ecb30
Piers Williams <pwillic@essex.ac.uk>
2014-11-19 13:33:12 +0000

    Actually added the files this time

---
+++ src/model/Entity.cpp
@@ -0,0 +1,18 @@
+/*
+ * Entity.cpp
+ *
+ *  Created on: 19 Nov 2014
+ *      Author: piers
+ */
+
+#include "Entity.h"
+
+Entity::Entity() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Entity::~Entity() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ src/model/Entity.h
@@ -0,0 +1,17 @@
+/*
+ * Entity.h
+ *
+ *  Created on: 19 Nov 2014
+ *      Author: piers
+ */
+
+#ifndef ENTITY_H_
+#define ENTITY_H_
+
+class Entity {
+public:
+	Entity();
+	virtual ~Entity();
+};
+
+#endif /* ENTITY_H_ */
