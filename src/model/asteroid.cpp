0e8592bdbf46cfd730ed12ddd21ae9114e727dc3
Piers Williams <pwillic@essex.ac.uk>
2014-11-20 17:00:11 +0000

    Improved Asteroid to use the non Box version of Set so should allow 5 points

--- src/model/asteroid.cpp
+++ src/model/asteroid.cpp
@@ -10,21 +10,27 @@ using namespace std;
 Asteroid::Asteroid(int points, float32 roughSize, float32 x, float32 y){
 
 	asteroidBody = 0; // Fixes warning about non-initialised member
-	this -> points.push_back(b2Vec2(0, 0));
+
+	b2Vec2 pointsArray [this -> points.size()];
+
+	// TODO - once I have worked how to correctly form the Asteroid the vector won't be needed
 	this -> points.push_back(b2Vec2(0, 1));
 	this -> points.push_back(b2Vec2(1, 1));
+	this -> points.push_back(b2Vec2(1.25, 0.5));
 	this -> points.push_back(b2Vec2(1, 0));
+	this -> points.push_back(b2Vec2(0, 0));
 
 	for(uint i = 0; i < this -> points.size(); i++){
 		this -> points[i] *= roughSize;
 		this -> points[i].x += x;
 		this -> points[i].y += y;
+		pointsArray[i] = this -> points[i];
 	}
 
 	bodyDef.type = b2_dynamicBody;
 	bodyDef.position.Set(x, y);
 
-	asteroidShape.SetAsBox(roughSize, roughSize);
+	asteroidShape.Set(pointsArray, points);
 
 	asteroidFixture.shape = &asteroidShape;
 	asteroidFixture.density = 1.0f; // TODO - Work out exactly what this means
