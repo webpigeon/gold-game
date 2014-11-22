b26c6bef13acfb03a7d6ff323b30df36a163d90e
Joseph Walton-Rivers <webpigeon@unitycoders.co.uk>
2014-11-22 01:22:16 +0000

    asteroids now render directly from their physics model

--- src/model/asteroid.cpp
+++ src/model/asteroid.cpp
@@ -66,16 +66,29 @@ void Asteroid::draw(SDL_GLContext* renderer){
 	b2Vec2 pos = asteroidBody->GetPosition();
 	float angle = asteroidBody->GetAngle();
 
+	//set shape
 	glColor3f(0, 1.0f, 0);
 	glPushMatrix();
+
+	//move the origin to the asteroid
 	glTranslatef(pos.x, pos.y, 0);
 	glRotatef(angle, 0, 0, 1);
+
+	// now we start drawing stuff
+
+	b2Fixture* f = asteroidBody->GetFixtureList();
 	glBegin(GL_POLYGON);
+	while (f) {
+		b2PolygonShape* shape = (b2PolygonShape*)f->GetShape();
+		int vertexCount = shape->GetVertexCount();
 
-	for(int i = 0; i < pointsLength; i++){
-		glVertex2f(pointsArray[i].x, pointsArray[i].y);
+		for (int i=0; i<vertexCount; ++i) {
+			b2Vec2 vertex = shape->GetVertex(i);
+			glVertex2f(vertex.x, vertex.y);
+		}
+
+		f = f->GetNext();
 	}
-
 	glEnd();
 
 	glPopMatrix();
