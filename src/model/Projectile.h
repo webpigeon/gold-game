aee23c575fe9ab230d706f3c1ae87e7b47ea5d8b
Piers Williams <pwillic@essex.ac.uk>
2014-11-23 17:13:52 +0000

    Started work on the Projectile class

---
+++ src/model/Projectile.h
@@ -0,0 +1,19 @@
+/*
+ * Projectile.h
+ *
+ *  Created on: 23 Nov 2014
+ *      Author: piers
+ */
+
+#ifndef PROJECTILE_H_
+#define PROJECTILE_H_
+
+#include "Entity.h"
+
+class Projectile: public Entity {
+public:
+	Projectile();
+	virtual ~Projectile();
+};
+
+#endif /* PROJECTILE_H_ */
