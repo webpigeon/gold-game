c65e79454b39b3b4a9375eea738093e27fcd5ec0
Piers Williams <pwillic@essex.ac.uk>
2014-11-24 11:54:02 +0000

    Start of Audio work

---
+++ src/model/Audio.cpp
@@ -0,0 +1,18 @@
+/*
+ * Audio.cpp
+ *
+ *  Created on: 24 Nov 2014
+ *      Author: piers
+ */
+
+#include "Audio.h"
+
+Audio::Audio() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Audio::~Audio() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ src/model/Audio.h
@@ -0,0 +1,20 @@
+/*
+ * Audio.h
+ *
+ *  Created on: 24 Nov 2014
+ *      Author: piers
+ */
+
+#ifndef AUDIO_H_
+#define AUDIO_H_
+
+#include "SDL2/SDL_mixer.h"
+
+class Audio {
+public:
+	Audio();
+	virtual ~Audio();
+
+};
+
+#endif /* AUDIO_H_ */
